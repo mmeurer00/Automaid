8195b66959edff65fd8515a8c08f5c73349e9e2b
Maxine <maxinemeurer@yahoo.com>
2021-04-22 19:55:02 -0400

    create sign up page in views and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,22 @@
 class UsersController < ApplicationController
-    def index
-    end
 
-    def show
-    end
-
+    #signing up
     def new
+        @user = User.new
     end
 
     def create
-    end
-
-    def edit
-    end
-
-    def update 
-    end
-
-    def delete
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to users_path
+        else
+            render :new
+        end
     end
 
     private
 
     def user_params
+        params.require(:user).permit(:name, :email, :password)
     end
 end
