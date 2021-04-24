012829a30b45d41a7f7547ab95b9a6d86d0a6dda
Maxine <maxinemeurer@yahoo.com>
2021-04-24 19:01:42 -0400

    connect device new page to show page, create form

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,6 @@
 class Device < ApplicationRecord
     has_many :automations
-    has_many: users
+    has_many :users
     has_many :automations, through: :user_devices
     has_many :users, through: :user_devices
 
