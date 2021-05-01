304616424d0fc61248c7e8f12278c7c7493180c1
Maxine <maxinemeurer@yahoo.com>
2021-05-01 15:32:09 -0400

    created time drop down menu

--- app/controllers/automations_controller.rb
+++ app/controllers/automations_controller.rb
@@ -1,6 +1,7 @@
 class AutomationsController < ApplicationController
     before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update]
     def new
+        @times = ["8:00 am","9:00 am","10:00 am", "11:00 am", "12:00 am", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm", "7:00 pm", "8:00 pm", "9:00 pm", "10:00 pm", "11:00 pm", "12:00 am"]
         device = current_user.devices.find_by(id: params[:device_id])
         @automation = device.automations.build
         #@automation.device = Device.find_by(id: params[:device_id])
