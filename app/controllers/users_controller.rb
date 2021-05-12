814b07a4a1df9a6410383c125f4d91c9706442fb
Maxine <maxinemeurer@yahoo.com>
2021-05-12 12:52:23 -0400

    extra styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:message] = "Successful signed up!"
             session[:user_id] = @user.id
             redirect_to rooms_path
         else
