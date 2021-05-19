a79c58cff724fd6d86fcbb152905d216fdce52c8
Maxine <maxinemeurer@yahoo.com>
2021-05-19 13:38:39 -0400

    application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def current_user
-        user ||= User.find(session[:user_id]) if session[:user_id].present?
+        @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
     end
 
     def logged_in?
